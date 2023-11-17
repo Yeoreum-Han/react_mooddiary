e8448d97d92301fc0ab896d0f12859ff826b7360
Yeoreum Han <yrh.summer@gmail.com>
2023-11-17 23:07:05 +0900

    DiaryForm_Emotions

--- src/components/DiaryForm.js
+++ src/components/DiaryForm.js
@@ -3,34 +3,29 @@ import Emotions from './Emotions';
 
 const DiaryForm = () => {
     return (
-        <div className='diaryForm'>
-            <div className='writeContentCover'>
-                <div className='emotionsCover'>
-                    {/* <div>긍정</div>
-                        데이터 따로 빼서 map으로 돌리기
-                    <div>부정</div> */}
-                    <p>#mood</p>
-                    <div className='emotionsPositive'>
-                        <Emotions/>
-                    </div>
-                    <div className='emotionsNegative'>
-                        <Emotions/>
-                    </div>
+        <div className='diaryFormCover'>
+            <div className='emotionsCover'>
+                <p>#mood</p>
+                <div className='emotionsPositive'>
+                    <Emotions emotions='positive' />
                 </div>
-                <div className='formCover'>
-                    <h2>오늘의 일기</h2>
-                    <form action='/write' method='post' id='form' >
-                        <input type='text' id='title' className='formTitle' placeholder='제목을 작성해주세요 (30자 이내)' />
-                        <input type='date' id='date' className='formDate' />
-                        <input type='text' id='mood' className='formMood' placeholder='오늘의 감정' />
-                        <textarea className='formText' />
-                        <div className='formButtons'>
-                            <div className='buttonCancel'>취소</div>
-                            <div className='buttonSave'>저장</div>
-                        </div>
+                <div className='emotionsNegative'>
+                    <Emotions emotions='negative' />
+                </div>
+            </div>
+            <div className='formCover'>
+                <h2>오늘의 일기</h2>
+                <form action='/write' method='post' id='form' >
+                    <input type='text' id='title' name='title' className='formTitle' placeholder='제목을 작성해주세요 (30자 이내)' />
+                    <input type='date' id='date' name='date' className='formDate' />
+                    <input type='text' id='mood' name='mood' className='formMood' placeholder='오늘의 감정' />
+                    <textarea id='text' name='text' className='formText' />
+                    <div className='formButtonsCover'>
+                        <div className='buttonCancel'>취소</div>
+                        <div className='buttonSave'>저장</div>
+                    </div>
 
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
     );
--- src/components/Emotions.js
+++ src/components/Emotions.js
@@ -1,16 +1,31 @@
 import './Emotions.css';
 
-const Emotions = () => {
+const Emotions = (props) => {
 
-    const emos = ['기쁨', '신남', '즐거움'];
+    const emos = ['기쁘다', '좋다', '만족스럽다', '흐뭇하다', '감격스럽다', '흥겹다', '가슴벅차다', '사랑스럽다', '열정있다', '온화하다', '애착이간다', '뿌듯하다', '믿음직하다', '갖고싶다', '활기차다'];
+    const emosNeg = ['화난다', '신경질난다', '싫다', '속상하다', '불쾌하다', '서운하다', '환멸난다', '답답하다', '상처받았다', '처량하다', '암담하다', '서럽다', '불안하다', '부끄럽다', '약오른다'];
+
+
     return (
         <>
             <div className='emotionsBox'>
-                {emos.map(emo => {
-                    return (
-                        <div>{emo}</div>
-                    );
-                })}
+                {props.emotions === 'positive' ?
+                    emos.map(emo => {
+                        return (
+                            <div className='emo'>
+                                <span>{emo}</span>
+                            </div>
+                        );
+                    })
+                    :
+                    emosNeg.map(emo => {
+                        return (
+                            <div className='emo'>
+                                <span>{emo}</span>
+                            </div>
+                        );
+                    })
+                }
 
             </div>
         </>
