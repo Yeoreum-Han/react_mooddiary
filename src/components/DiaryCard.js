e1a77a39944d6f640924e6f0d70d64b2e322b2a5
Yeoreum Han <yrh.summer@gmail.com>
2023-11-20 23:00:42 +0900

    postDelete

--- src/components/DiaryCard.js
+++ src/components/DiaryCard.js
@@ -1,6 +1,26 @@
+import axios from 'axios';
 import './DiaryCard.css';
+import { useNavigate } from 'react-router-dom';
 
-const DiaryCard = ({ title, date, mood, text }) => {
+const DiaryCard = ({ id, title, date, mood, text , getPosts }) => {
+
+    const navigate = useNavigate();
+
+    const getConfirm = (id) => {
+        if (window.confirm('삭제하겠습니까?')) {
+            axios.delete(`http://localhost:5000/posts/${id}`)
+                .then(() => {
+                    alert('삭제됨!');
+                    getPosts();
+                });
+        } else {
+            alert('삭제안됨');
+        };
+    };
+
+    const toEdit = (id) => {
+        navigate(`/:${id}/edit`);
+    }
 
     return (
         <div className="diaryCardCover">
@@ -11,8 +31,8 @@ const DiaryCard = ({ title, date, mood, text }) => {
             </div>
             <div className='cardText'>{text}</div>
             <div className='cardButtonsCover'>
-                <div className='buttonEdit' />
-                <div className='buttonDelete'/>
+                <div className='buttonEdit' onClick={()=> toEdit(id)}/>
+                <div className='buttonDelete' onClick={() => getConfirm(id)} />
             </div>
         </div>
 
--- src/components/ShowDiaries.js
+++ src/components/ShowDiaries.js
@@ -7,23 +7,25 @@ const ShowDiaries = () => {
 
     const [posts, setPosts] = useState([]);
 
+    //처음에 한번 데이터 가져오기
     useEffect(() => {
         getPosts();
-    }, []);
+    },[]);
 
     const getPosts = () => {
         axios.get('http://localhost:5000/posts',).then((res) => {
             setPosts(res.data);
         });
     };
+    
 
     const renderDiaryCards = () => {
         return posts.map((post) => {
             return <DiaryCard
-                key={post.id} title={post.title} date={post.date} mood={post.mood} text={post.text} />
+                id={post.id} title={post.title} date={post.date} mood={post.mood} text={post.text} getPosts={getPosts}/>
         });
     };
-    
+
     return (
         <div className='showDiariesCover'>
             {renderDiaryCards()}
