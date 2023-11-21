122dd9609fb5a82310a942e9c611f9a908ed95ae
Yeoreum Han <yrh.summer@gmail.com>
2023-11-21 18:28:33 +0900

    loadingSpinner

--- src/components/ShowDiaries.js
+++ src/components/ShowDiaries.js
@@ -2,30 +2,36 @@ import './ShowDiaries.css';
 import DiaryCard from "./DiaryCard";
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import LoadingSpinner from './LoadingSpinner';
 
 const ShowDiaries = () => {
 
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     //처음에 한번 데이터 가져오기
     useEffect(() => {
         getPosts();
     },[]);
-
+    
+    
     const getPosts = () => {
         axios.get('http://localhost:5000/posts',).then((res) => {
             setPosts(res.data);
+            setLoading(false);
         });
     };
     
-
+    
     const renderDiaryCards = () => {
         return posts.map((post) => {
             return <DiaryCard
-                id={post.id} title={post.title} date={post.date} mood={post.mood} text={post.text} getPosts={getPosts}/>
+            id={post.id} title={post.title} date={post.date} mood={post.mood} text={post.text} getPosts={getPosts}/>
         });
     };
-
+    
+    if(loading) { return <LoadingSpinner/>}
+    
     return (
         <div className='showDiariesCover'>
             {renderDiaryCards()}
