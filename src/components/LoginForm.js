a2cc377096aa4abd7322449b7d006e4bce0ab40a
Yeoreum Han <yrh.summer@gmail.com>
2023-11-24 18:09:09 +0900

    Login_bgClose

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,43 @@
 import './LoginForm.css';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const LoginForm = ({isOpen, closeLogin}) => {
-    return(
-        <div className='loginFormCover' style={{display : isOpen ? 'block' : 'none'}}>
-            <div>로그인모달창</div>
-            <div onClick={closeLogin}>close버튼</div>
+const LoginForm = ({ isOpen, closeLogin }) => {
+
+    const ref = useRef(null);
+    const navigate = useNavigate();
+
+    const modalClose = (e) => {
+        if(ref.current === e.target) {
+            closeLogin();
+        }
+    };
+
+    const moveToCreate = () => {
+        closeLogin();
+        navigate('/createAccount');
+    }
+
+    return (
+        <div className='loginFormBg' style={{ display: isOpen ? 'block' : 'none' }}  onClick={(e)=>{modalClose(e)}} ref={ref}>
+            <div className='loginFormCover'>
+                <div className='loginTitle'>
+                    <p>Log In</p>
+                </div>
+                <form className='loginForm'>
+                    <div className='userInfo'>
+                        <input type='text' id='userId' placeholder='username' className='userId' />
+                        <input type='password' id='userPwd' placeholder='password' className='userPwd' />
+                    </div>
+                    <div className='loginButton'>Log In</div>
+                </form>
+                <div className='createAccountCover'>
+                    회원이 아니신가요?
+                    <div className='createAccount' onClick={moveToCreate}>
+                        계정 만들기
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
