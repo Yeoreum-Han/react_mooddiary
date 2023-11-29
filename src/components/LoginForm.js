504f7505c3a6437f01dc3810d46d1bbe9f8ca72d
Yeoreum Han <yrh.summer@gmail.com>
2023-11-29 17:19:42 +0900

    bcyrpt

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import './LoginForm.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+const bcyrpt = require('bcryptjs');
 
 const LoginForm = ({ isOpen, closeLogin }) => {
 
@@ -11,12 +12,13 @@ const LoginForm = ({ isOpen, closeLogin }) => {
 
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPwd, setLoginPwd] = useState('');
+    const [userInfo, setUserInfo] = useState({});
 
-    const [cookies, setCookie] = useCookies(['accessToken']);
+    const [cookies, setCookie] = useCookies();
 
 
     const modalClose = (e) => {
-        if(ref.current === e.target) {
+        if (ref.current === e.target) {
             closeLogin();
         }
     };
@@ -26,24 +28,47 @@ const LoginForm = ({ isOpen, closeLogin }) => {
         navigate('/createAccount');
     }
 
-    const login = () => {
-        axios.post('http://localhost:5000/login', { email : loginEmail, password : loginPwd})
-        .then((res)=>{
-            closeLogin();
-            setCookie('accessToken', res.data.accessToken);
+    useEffect(() => {
+        getUserInfo()
+    }, []);
+
+    const getUserInfo = () => {
+        axios.get('http://localhost:5000/users', {
+        }).then((res) => {
+            setUserInfo(res.data[0]);
         });
+    };
+
+
+    const login = async () => {
+        //bcyrpt.compare로 비밀번호 비교해서 t/f return
+        const validPwd = await bcyrpt.compare(loginPwd, userInfo.password);
+
+        if (loginEmail === userInfo.email) {
+            if(validPwd){
+                axios.post('http://localhost:5000/login', { email: loginEmail, password: loginPwd })
+                    .then((res) => {
+                        closeLogin();
+                        setCookie('accessToken', res.data.accessToken);
+                    });
+            } else {
+                alert('비밀번호가 일치하지 않습니다');
+            }
+        } else {
+            alert('이메일 또는 비밀번호를 확인해주세요');
+        }
     }
 
     return (
-        <div className='loginFormBg' style={{ display: isOpen ? 'block' : 'none' }}  onClick={(e)=>{modalClose(e)}} ref={ref}>
+        <div className='loginFormBg' style={{ display: isOpen ? 'block' : 'none' }} onClick={(e) => { modalClose(e) }} ref={ref}>
             <div className='loginFormCover'>
                 <div className='loginTitle'>
                     <p>Log In</p>
                 </div>
                 <form className='loginForm'>
                     <div className='userInfo'>
-                        <input type='email' id='loginEmail' name='loginEmail' placeholder='userEmail' className='userId' value={loginEmail} onChange={(e)=>{setLoginEmail(e.target.value)}}/>
-                        <input type='password' id='loginPwd' name='loginPwd' placeholder='password' className='userPwd' value={loginPwd} onChange={(e)=>{setLoginPwd(e.target.value)}} />
+                        <input type='email' id='loginEmail' name='loginEmail' placeholder='userEmail' className='userId' value={loginEmail} onChange={(e) => { setLoginEmail(e.target.value) }} />
+                        <input type='password' id='loginPwd' name='loginPwd' placeholder='password' className='userPwd' value={loginPwd} onChange={(e) => { setLoginPwd(e.target.value) }} />
                     </div>
                     <div className='loginButton' onClick={login}>Log In</div>
                 </form>
